9b077ea1865cf2526671caa18b686cbfe4eef9e1
ashwanthdurairaj <ashwanthdurairaj2003@gmail.com>
2023-07-03 18:43:31 +0530

    second commit

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended : false}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.use('/api/goals', require('./backend/routes/goalRoutes')); //route
 app.use('/api/users', require('./backend/routes/userRoutes'));
 app.use('/api/billing', require('./backend/routes/billing'));  
